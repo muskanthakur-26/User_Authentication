062af38fe2ebd649a9c0ff86064542c561aff996
Muskan <76208395+muskanthakur-26@users.noreply.github.com>
2022-02-01 13:27:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Running");
+function speakText(){
+  var text = 'Thank you for registering !';
+  responsiveVoice.speak(text);
+  alert("You are a registered user now!")
+}
+function speakText2(){
+  var text ='Logged in !';
+  responsiveVoice.speak(text);
+  alert("You are successfully logged into your account!")
+}
